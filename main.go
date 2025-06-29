a86f300d47be11674a0f8dbda50db60023126286
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-06-29 23:26:03 +0200

    Simple server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Starting redis server...")
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection:", err)
+		return
+	}
+	conn.Write([]byte("Ping to redis server!\r\n"))
+	defer conn.Close()
 }
