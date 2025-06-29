f8d6b5279403264bc1bbfda9fbd9ec54461aab00
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-06-29 23:40:14 +0200

    Simple server with OK response

--- main.go
+++ main.go
@@ -2,21 +2,41 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 func main() {
+	port := "6379"
 	fmt.Println("Starting redis server...")
-	l, err := net.Listen("tcp", ":6379")
+	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 		return
 	}
+	fmt.Printf("Listening on port %s.\n", port)
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection:", err)
 		return
 	}
-	conn.Write([]byte("Ping to redis server!\r\n"))
+
 	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		_, err = conn.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Connection closed by client.")
+				break
+			}
+			fmt.Println("Error reading from connection:", err.Error())
+			os.Exit(1)
+		}
+		// for now ignore request and send OK response
+		conn.Write([]byte("+OK\r\n"))
+		fmt.Println("Received request, sent OK response.")
+	}
 }
