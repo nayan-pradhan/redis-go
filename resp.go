b11b04e72122f801a6b7134c04c22d0d8f98eb1f
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-01 23:51:18 +0200

    Function to read line

--- resp.go
+++ resp.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 )
 
@@ -27,4 +28,20 @@ type RESP struct {
 
 func NewRESP(rd io.Reader) *RESP {
 	return &RESP{reader: bufio.NewReader(rd)} // Create a new RESP instance with a buffered reader
+}
+
+func (r *RESP) readValue() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break // Check if the last two bytes are \r\n, indicating end of line
+		}
+		fmt.Println("Read byte:", b, "Current line:", string(line), "length:", len(line))
+	}
+	return line, n, nil
 }
