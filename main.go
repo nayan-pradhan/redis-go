f214b2f41deb71f0edaf6438d3d92c6b5f0ed7cb
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-01 23:51:45 +0200

    Updating main to test readValue func

--- main.go
+++ main.go
@@ -2,41 +2,49 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net"
 	"os"
 )
 
 func main() {
-	port := "6379"
-	fmt.Println("Starting redis server...")
-	l, err := net.Listen("tcp", ":"+port)
+	r := NewRESP(os.Stdin)        // Initialize RESP with standard input
+	line, n, err := r.readValue() // Read a value from the RESP protocol
+
 	if err != nil {
-		fmt.Println("Error starting server:", err)
-		return
-	}
-	fmt.Printf("Listening on port %s.\n", port)
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection:", err)
+		fmt.Println("Error reading value:", err)
 		return
 	}
 
-	defer conn.Close()
+	fmt.Printf("Read %d bytes: %s\n", n, string(line)) // Print the number of bytes read and the value
 
-	for {
-		buf := make([]byte, 1024)
-		_, err = conn.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				fmt.Println("Connection closed by client.")
-				break
-			}
-			fmt.Println("Error reading from connection:", err.Error())
-			os.Exit(1)
-		}
-		// for now ignore request and send OK response
-		conn.Write([]byte("+OK\r\n"))
-		fmt.Println("Received request, sent OK response.")
-	}
+	// port := "6379"
+	// fmt.Println("Starting redis server...")
+	// l, err := net.Listen("tcp", ":"+port)
+	// if err != nil {
+	// 	fmt.Println("Error starting server:", err)
+	// 	return
+	// }
+	// fmt.Printf("Listening on port %s.\n", port)
+	// conn, err := l.Accept()
+	// if err != nil {
+	// 	fmt.Println("Error accepting connection:", err)
+	// 	return
+	// }
+
+	// defer conn.Close()
+
+	// for {
+	// 	buf := make([]byte, 1024)
+	// 	_, err = conn.Read(buf)
+	// 	if err != nil {
+	// 		if err == io.EOF {
+	// 			fmt.Println("Connection closed by client.")
+	// 			break
+	// 		}
+	// 		fmt.Println("Error reading from connection:", err.Error())
+	// 		os.Exit(1)
+	// 	}
+	// 	// for now ignore request and send OK response
+	// 	conn.Write([]byte("+OK\r\n"))
+	// 	fmt.Println("Received request, sent OK response.")
+	// }
 }
