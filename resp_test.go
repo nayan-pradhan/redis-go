5dd3adf2ef67afb19369e005bc8033a62ae39684
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-04 00:20:37 +0200

    More tests

--- resp_test.go
+++ resp_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -62,5 +63,59 @@ func TestMarshalArray(t *testing.T) {
 	result := v.marshalArray()
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("marshalArray failed: got %q, want %q", result, expected)
+	}
+}
+
+func TestRESPReadString(t *testing.T) {
+	input := "+OK\r\n"
+	r := NewRESP(strings.NewReader(input))
+	v, err := r.Read()
+	if err != nil {
+		t.Fatalf("Read error: %v", err)
+	}
+	if v.typ != "string" || v.str != "OK" {
+		t.Errorf("Read string: got typ=%q, str=%q; want typ=\"string\", str=\"OK\"", v.typ, v.str)
+	}
+}
+
+func TestRESPReadInteger(t *testing.T) {
+	input := ":123\r\n"
+	r := NewRESP(strings.NewReader(input))
+	v, err := r.Read()
+	if err != nil {
+		t.Fatalf("Read error: %v", err)
+	}
+	if v.typ != "integer" || v.num != 123 {
+		t.Errorf("Read integer: got typ=%q, num=%d; want typ=\"integer\", num=123", v.typ, v.num)
+	}
+}
+
+func TestRESPReadBulk(t *testing.T) {
+	input := "$5\r\nhello\r\n"
+	r := NewRESP(strings.NewReader(input))
+	v, err := r.Read()
+	if err != nil {
+		t.Fatalf("Read error: %v", err)
+	}
+	if v.typ != "bulk" || v.bulk != "hello" {
+		t.Errorf("Read bulk: got typ=%q, bulk=%q; want typ=\"bulk\", bulk=\"hello\"", v.typ, v.bulk)
+	}
+}
+
+func TestRESPReadArray(t *testing.T) {
+	input := "*2\r\n+foo\r\n$3\r\nbar\r\n"
+	r := NewRESP(strings.NewReader(input))
+	v, err := r.Read()
+	if err != nil {
+		t.Fatalf("Read error: %v", err)
+	}
+	if v.typ != "array" || len(v.array) != 2 {
+		t.Errorf("Read array: got typ=%q, len=%d; want typ=\"array\", len=2", v.typ, len(v.array))
+	}
+	if v.array[0].typ != "string" || v.array[0].str != "foo" {
+		t.Errorf("Read array[0]: got typ=%q, str=%q; want typ=\"string\", str=\"foo\"", v.array[0].typ, v.array[0].str)
+	}
+	if v.array[1].typ != "bulk" || v.array[1].bulk != "bar" {
+		t.Errorf("Read array[1]: got typ=%q, bulk=%q; want typ=\"bulk\", bulk=\"bar\"", v.array[1].typ, v.array[1].bulk)
 	}
 }
