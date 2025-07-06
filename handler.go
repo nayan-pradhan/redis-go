533313bc4833228a71ed82989312605728aee1c5
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-06 23:32:53 +0200

    Updating ping to return PONG or first arg

--- handler.go
+++ handler.go
@@ -1,10 +1,18 @@
-var Handlers = map[string]func([]Value) Value {
-	"PING": ping
+package main
+
+var Handlers = map[string]func([]Value) Value{
+	"PING": ping,
 }
 
 func ping(args []Value) Value {
+	if len(args) == 0 {
+		return Value{
+			typ: "string",
+			str: "PONG",
+		}
+	}
 	return Value{
 		typ: "string",
-		str: "PONG",
+		str: args[0].bulk, // Return the first argument as the response
 	}
 }
