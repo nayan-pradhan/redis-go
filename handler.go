1ca2ec0e88f98c2ccbf1aedb662dc703fd1936dd
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-07 00:07:49 +0200

    Skeleton for set and get handlers

--- handler.go
+++ handler.go
@@ -1,8 +1,15 @@
 package main
 
+import "sync"
+
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
+	"SET":  set,
+	"GET":  get,
 }
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{} // using sync.RWMutex because our server should handle requests concurrently
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -15,4 +22,12 @@ func ping(args []Value) Value {
 		typ: "string",
 		str: args[0].bulk, // Return the first argument as the response
 	}
+}
+
+func set(args []Value) Value {
+	return Value{}
+}
+
+func get(args []Value) Value {
+	return Value{}
 }
