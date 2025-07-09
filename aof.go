d73b7d44ad859f0001d5fa5592f0fdcd1a084afa
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-09 22:31:55 +0200

    Defining Aof struct

--- aof.go
+++ aof.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"bufio"
+	"os"
+	"sync"
+)
+
 // AOF: Append Only File - we record each command in a file as RESP.
 // When a restart occurs, Redis reads all RESP commands from teh AOF and executes them in memory for data persistency.
+
+type Aof struct {
+	file *os.File
+	rd   *bufio.Reader
+	mu   sync.Mutex
+}
