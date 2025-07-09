3a8b3114f8c7df98579669ddddde4363f4cf038a
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-09 22:42:25 +0200

    Implementation for new aof creation and closing aof

--- aof.go
+++ aof.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"sync"
+	"time"
 )
 
 // AOF: Append Only File - we record each command in a file as RESP.
@@ -13,4 +14,35 @@ type Aof struct {
 	file *os.File
 	rd   *bufio.Reader
 	mu   sync.Mutex
+}
+
+func NewAof(path string) (*Aof, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666) // creates file if doesnt exist, opens if exists. 0666 is read/write permissions for everyone
+	if err != nil {
+		return nil, err
+	}
+
+	aof := &Aof{
+		file: f,
+		rd:   bufio.NewReader(f),
+	}
+
+	// start a go routine to sync AOF to disk every 1 second
+	go func() {
+		for {
+			aof.mu.Lock()
+			aof.file.Sync()
+			aof.mu.Unlock()
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return aof, nil
+}
+
+func (aof *Aof) Close() error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	return aof.file.Close()
 }
