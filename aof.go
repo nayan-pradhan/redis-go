47a26663a1377211a6ee8e42c1bc7dd1f5403a3b
nayan-pradhan <nayan.pradhan@hotmail.com>
2025-07-09 22:03:19 +0200

    Adding comment

--- aof.go
+++ aof.go
@@ -1,1 +1,4 @@
 package main
+
+// AOF: Append Only File - we record each command in a file as RESP.
+// When a restart occurs, Redis reads all RESP commands from teh AOF and executes them in memory for data persistency.
